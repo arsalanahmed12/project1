b55f7fc1450c26d751e5fce14f515d0bb2ce8daf
arsalanahmed12 <khanarsal884@gmail.com>
2024-03-28 01:29:58 +0500

    project ticket counter complete

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Welcome To The Karachi Park! Please Enter Your Age For Generate Ticket", type: "number", name: "ticket" }
+]);
+if (answer.ticket >= 0 && answer.ticket <= 4) {
+    console.log("Welcome, Kids! Free tickets for fun-filled joy in the park!");
+}
+else if (answer.ticket >= 5 && answer.ticket <= 17) {
+    console.log("Welcome, purchase ticket in just RS:200/= and make your day fun-filled!");
+}
+else if (answer.ticket >= 18 && answer.ticket <= 49) {
+    console.log("Welcome, purchase ticket in just RS:400/= and make your day fun-filled");
+}
+else if (answer.ticket >= 50) {
+    console.log("Welcome seniors! Enjoy complimentary access to our Park!");
+}
+else {
+    console.log("Please enter valid age");
+}
